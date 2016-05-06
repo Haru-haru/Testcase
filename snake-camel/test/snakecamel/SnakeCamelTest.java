86ea76710f36b3bed04bc56021814f490b896fae
g14922kh <g14922kh@s30416.tsuda.ac.jp>
2016-05-06 11:19:28 +0900

    テストクラスを作ったよ！

---
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -0,0 +1,13 @@
+package snakecamel;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SnakeCamelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
