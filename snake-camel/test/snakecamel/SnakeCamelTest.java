cfc5ae2e3bceccf3810dd9ff89d5c8c56d3489c1
g14922kh <g14922kh@s30521.tsuda.ac.jp>
2016-05-09 13:43:28 +0900

    テスーケース作りました

--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -1,13 +1,49 @@
 package snakecamel;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class SnakeCamelTest {
-
+public class SnakeCamelTest{
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void snakeからcamelに直すice_creamをIceCreamに(){
+		String expected="IceCream";
+		String actual=SnakeCamelUtil.snakeToCamelcase("ice_cream");
+		assertThat(actual,is(expected));
 	}
 
+	@Test
+	public void camelからsnakeに直すILoveYouをi_love_youに(){
+		String expected="i_love_you";
+		String actual=SnakeCamelUtil.camelToSnakecase("ILoveYou");
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void capitalize大文字にするaをAに(){
+		String expected="A";
+		String actual=SnakeCamelUtil.capitalize("a");
+		assertThat(actual,is(expected));
+		
+	}
+	@Test
+	public void capitalize大文字にするhariboをHariboに(){
+		String expected="Haribo";
+		String actual=SnakeCamelUtil.capitalize("haribo");
+		assertThat(actual,is(expected));
+		
+	}
+	@Test
+	public void uncapitalize小文字に直すBをbに(){
+		String expected="b";
+		String actual=SnakeCamelUtil.uncapitalize("B");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void uncapitalize小文字に直すHariboをhariboに(){
+		String expected="haribo";
+		String actual=SnakeCamelUtil.uncapitalize("Haribo");
+		assertThat(actual,is(expected));
+	}
 }
